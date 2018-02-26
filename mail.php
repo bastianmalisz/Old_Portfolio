796295c941ace913a2194330a90bcc4012b7d65d
bastianmalisz <malisz654321@gmail.com>
2018-02-26 21:18:35 +0100

    s

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['wiadomosc'];
 $formcontent="Od: $name \n Wiadomość: $message";
-$recipient = "maliszewskiseba@protonmail.com";
+$recipient = "malisz654321@gmail.com";
 $subject = "Informacja z formularza kontaktowego";
 $mailheader = "Od: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
